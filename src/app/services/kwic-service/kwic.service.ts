3327f20481cc11010d18e34afa2819b715aaaa9a
chinthoorie <cyogalingam@gmail.com>
2019-11-30 00:26:58 -0600

    service integration

--- src/app/services/kwic-service/kwic.service.ts
+++ src/app/services/kwic-service/kwic.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {DataKey, DataStore} from '../../util/data-store.service';
 
 @Injectable()
 export class KwicService {
 
   private usersUrl: string;
+  private wordsUrl: string;
+  private resultsUrl: string;
 
-  constructor(private http: HttpClient) {
-    this.usersUrl = 'http://localhost:8080/kwic/alphabetizer';
+  constructor(private http: HttpClient, private dataStore: DataStore) {
+    this.usersUrl = 'http://localhost:8081/kwic/alphabetizer';
+    this.wordsUrl = 'http://localhost:8081/kwic/autofill';
+    this.resultsUrl = 'http://localhost:8081/kwic/search';
   }
 
   public indexInput(lines: string[]): Observable<string[][]> {
@@ -16,6 +21,34 @@ export class KwicService {
     let params = new HttpParams();
     params = params.append('lines', lines[0]);
 
-    return this.http.get<string[][]>(this.usersUrl, {params});
+    this.http.get<string[][]>(this.usersUrl, {params}).subscribe( res => {
+      this.dataStore.set(DataKey.shiftedLines, res);
+    });
+
+    return this.dataStore.get(DataKey.shiftedLines, true);
+  }
+
+  public getWord(word: string) {
+
+    let params = new HttpParams();
+    params = params.append('word', word);
+
+    this.http.get<string[]>(this.wordsUrl, {params}).subscribe( res => {
+      this.dataStore.set(DataKey.autofillWords, res);
+    });
+
+    return this.dataStore.get(DataKey.autofillWords, true);
+  }
+
+  public getSearchResults(line: string): Observable<string[]> {
+
+    let params = new HttpParams();
+    params = params.append('line', line);
+
+    this.http.get<string[]>(this.resultsUrl, {params}).subscribe( res => {
+      this.dataStore.set(DataKey.urls, res);
+    });
+
+    return this.dataStore.get(DataKey.urls, true);
   }
 }
